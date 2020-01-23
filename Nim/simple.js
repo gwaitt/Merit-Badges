aa4118a89a8678441bd3b4289a693a415da6be8e
gwaitt <54862354+gwaitt@users.noreply.github.com>
2020-01-23 14:17:11 -0500

    Create simple.js

---
+++ Nim/simple.js
@@ -0,0 +1,56 @@
+/* Nim Super Simple */
+
+/* Globals 
+ * count = integer, winner = String
+ */ 
+var again = true
+if (again = true){
+  var count = 0 var turn = 0 var winner = null;
+  while (var count < 21){
+  cpuTurn()
+    count += turn;
+    count = (cpuCount + turn )
+             while(count<21){
+      userTurn()
+               count += turn
+               alert("count is not "+count+")
+                     winner = computer
+    }
+  }
+ /* Main Code 
+  * loop while count <21
+  * if count < 21 call cpuTurn, adding to count and alerting count. Set winner as player. 
+  * if count < 21 call playerTurn, adding to count and alerting count. Set winner as CPU.
+  * end loop
+  * alert winner
+  */
+  while (count <= 21){
+    cpuTurn = (Math.floor(Math.random)*3)+1
+    count += turn
+    
+  }
+  
+  function(turn)
+  turn = prompt("How many do you count?");
+  if(turn > 3){
+    alert("too high, cheater!");
+    else return turn
+  }
+  
+  function (cpuTurn){
+cpuCount = Math.floor(Math.random(*3)+1
+   while (turn+count > 20 && turn>1)
+    turn()
+/* playerTurn
+ * set winner as CPU
+ * prompt for turn (1-3)
+ * @param none
+ * @return turn (integer)
+ */
+
+/* cpuTurn
+ * set winner as player
+ * set turn as random number 1-3
+ * alert CPU's turn
+ * return turn
+ */
